6e0b0b6447f64ffdbfc6adc9d5ef4e38f9106285
Polo <polodealvarado@gmail.com>
2019-03-09 13:21:30 +0100

    Change of tittle panel

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("WorldBank"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
