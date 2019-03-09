c7d81f097c583168730203171a23badd0827c524
manugaco <manu.garcia.corbi@gmail.com>
2019-03-09 14:44:36 +0100

    dataset library and tidying

--- Tyding_datasets.R
+++ Tyding_datasets.R
@@ -1,13 +1,7 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
+
+#Loading libraries
+
 library(tidyverse)
 library(WDI)
 library(wbstats)
@@ -18,11 +12,11 @@ library(progress)
 #Vqriables of interest (codes and names)
 
 indicators_code <- c("NY.GDP.PCAP.CD", "NY.GDP.DEFL.KD.ZG", "BN.GSR.GNFS.CD", "NE.TRD.GNFS.ZS", 
-                     "LP.LPI.INFR.XQ", "SL.UEM.TOTL.ZS", "SP.POP.TOTL", "SL.TLF.TOTL.IN", "NE.DAB.TOTL.ZS", "NE.CON.TOTL.ZS",
-                     "GC.DOD.TOTL.GD.ZS", "SP.DYN.LE00.IN", "SP.DYN.CBRT.IN", "SP.DYN.CDRT.IN", "SE.SEC.TCAQ.UP.ZS", "SE.COM.DURS",
-                     "SE.XPD.TOTL.GD.ZS", "SE.ADT.LITR.ZS", "SE.PRM.REPT.ZS", "SE.SEC.PROG.ZS", "ST.INT.ARVL", "GC.TAX.IMPT.ZS",
-                     "LP.LPI.CUST.XQ", "TM.VAL.FUEL.ZS.UN", "TX.VAL.TECH.MF.ZS", "GB.XPD.RSDV.GD.ZS", "IP.JRN.ARTC.SC", 
-                     "SP.POP.SCIE.RD.P6", "IP.PAT.RESD", "IP.PAT.NRES")
+"LP.LPI.INFR.XQ", "SL.UEM.TOTL.ZS", "SP.POP.TOTL", "SL.TLF.TOTL.IN", "NE.DAB.TOTL.ZS", "NE.CON.TOTL.ZS",
+"GC.DOD.TOTL.GD.ZS", "SP.DYN.LE00.IN", "SP.DYN.CBRT.IN", "SP.DYN.CDRT.IN", "SE.SEC.TCAQ.UP.ZS", "SE.COM.DURS",
+"SE.XPD.TOTL.GD.ZS", "SE.ADT.LITR.ZS", "SE.PRM.REPT.ZS", "SE.SEC.PROG.ZS", "ST.INT.ARVL", "GC.TAX.IMPT.ZS",
+"LP.LPI.CUST.XQ", "TM.VAL.FUEL.ZS.UN", "TX.VAL.TECH.MF.ZS", "GB.XPD.RSDV.GD.ZS", "IP.JRN.ARTC.SC", 
+"SP.POP.SCIE.RD.P6", "IP.PAT.RESD", "IP.PAT.NRES")
 
 indicators_name <- c("GDP per capita (current US$)","Gross national expenditure (% of GDP)", "Inflation, GDP deflator (annual %)", 
                      "Final consumption expenditure (% of GDP)", "Central government debt, total (% of GDP)",
@@ -53,11 +47,11 @@ list <- list()
 pb <- progress_bar$new(total = 30)
 
 for(i in 1:length(indicators_code)){
-  pb$tick()
-  indicator <- indicators_name[i]
-  var <- WDI(country = "all", indicator = c(indicator =  indicators_code[i]), start = 1960,
+   pb$tick()
+   indicator <- indicators_name[i]
+   var <- WDI(country = "all", indicator = c(indicator =  indicators_code[i]), start = 1960,
              end = 2018, extra = TRUE)
-  list[[i]] <- var %>% spread(year, indicator)
+   list[[i]] <- var %>% spread(year, indicator)
 }
 
 #Naming the list and selecting country names only
@@ -93,44 +87,3 @@ arrests$region <- tolower(rownames(USArrests))
 
 choro <- merge(states, arrests, sort = FALSE, by = "country")
 choro <- choro[order(choro$order), ]
-
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
-
