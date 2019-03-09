13ae8ab6e2ba9ef7ece873b2eafd83ea00cb194f
Polo <polodealvarado@gmail.com>
2019-03-09 19:44:58 +0100

    app and Timeseries

--- app.R
+++ app.R
@@ -7,43 +7,35 @@
 #    http://shiny.rstudio.com/
 #
 
+# Different modules of our application
+
 library(shiny)
+library(shinythemes)
+library(shinydashboard)
+library(markdown)
+
+# Image URL 
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+ui <- fluidPage(theme = shinytheme("superhero"),
+                
+                navbarPage(title=div(div(id = "img-id",img(src="wb.png")),"WORLD BANK"),
+                           tabPanel("TIME SERIE",icon=icon("fas fa-chart-line")),
+                           tabPanel("WORLD MAP",icon=icon("far fa-globe")),
+                           tabPanel("SCATTER PLOT",icon=icon("bar-chart-o")),
+                           navbarMenu("INFO",icon=icon("far fa-info"),
+                                      tabPanel("PARTICIPANTS",icon=icon("fas fa-user")),
+                                      tabPanel("DATASET",icon=icon("fas fa-database")))
+                )
+                
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
 }
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
